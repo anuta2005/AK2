7cef4dce8fe14c51760c51d85e3faf5fe92b8e74
Anna <fyxrffufif@mail.ru>
2020-11-17 11:57:05 +0300

    Программа1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PyQt5.QtGui import QPainter, QColor
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        uic.loadUi('addEditCoffeeForm.ui', self)
         self.con = sqlite3.connect("coffee1.sqlite")
         cur = self.con.cursor()
         result = cur.execute("""SELECT * FROM coffees""").fetchall()
@@ -18,6 +18,16 @@ class MyWidget(QMainWindow):
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.pushButton.clicked.connect(self.run)
+        self.pushButton_2.clicked.connect(self.run1)
+
+    def run(self):
+        rowPosition = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(rowPosition)
+
+    def run1(self):
+        cur = self.con.cursor()
+        self.con.commit()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
